d2a6048b2fa77740aeba5260b177c8930fb37e7d
Sujith PS <sujith@kiebot.com>
2022-11-14 11:14:26 +0530

    Sujith | Add product list view page

--- src/main/java/in/upcode/eshop/controller/ProductController.java
+++ src/main/java/in/upcode/eshop/controller/ProductController.java
@@ -1,19 +1,31 @@
 package in.upcode.eshop.controller;
 
 import in.upcode.eshop.model.Product;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
-@RestController
+@Controller
 public class ProductController {
+    final List<Product> products = List.of(
+            new Product(1, "Shoe", 10),
+            new Product(2, "Book", 100),
+            new Product(3, "Computer", 3)
+    );
 
-    @GetMapping("products")
+    @RequestMapping("products")
+    @ResponseBody
     public List<Product> getAllProducts() {
-        return List.of(
-                new Product(1, "Shoe", 10),
-                new Product(2, "Book", 100)
-        );
+        return products;
+    }
+
+    @RequestMapping("view/products")
+    public String displayProducts(Model model) {
+        model.addAttribute("products", products);
+
+        return "products";
     }
 }
